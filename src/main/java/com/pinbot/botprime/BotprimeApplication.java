f85fee61f1df0322916f0a2f81656650c053e23a
PinBot <lll24la@gmail.com>
2025-06-15 15:21:51 +0300

    Первоначальное развертывание проекта

---
+++ src/main/java/com/pinbot/botprime/BotprimeApplication.java
@@ -0,0 +1,13 @@
+package com.pinbot.botprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotprimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotprimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinbot/botprime/BotprimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinbot.botprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotprimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
